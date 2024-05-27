c8cc73fad6f6eafb0fbb850297092e3ef55991b8
Ruslan Mahats <ruslanmahats13@gmail.com>
2024-05-27 17:06:28 +0300

    add url

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('ws://chat-app-dg.vercel.app')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
